58dcd112a7b021ad8fffc827951672f3c06efd18
comeonsunny <2286323940@qq.com>
2023-09-25 18:56:34 -0400

    The correct version for the update the number of the hints generated via the key_mac

--- MQPIR/src/client.rs
+++ MQPIR/src/client.rs
@@ -9,14 +9,14 @@ use crate::globals;
 
 pub fn preprocess() {
 
-    let mut keys = [0; globals::NUM_OF_HINTS * globals::KEY_SIZE];
+    let mut keys = [0; globals::NUM_OF_HINTS * globals::KEY_SIZE * 2];
     let mut rng = OsRng;
     rng.fill_bytes(&mut keys);
 
     let mut stream = TcpStream::connect(format!("{}:{}", globals::IP_ADDRESS, globals::PORT)).unwrap();
     let _ = stream.write(&keys).unwrap();
 
-    let mut buf = [0; globals::NUM_OF_HINTS * globals::SQRT_N];
+    let mut buf = [0; globals::NUM_OF_HINTS * globals::SQRT_N * globals::BLOCK_SIZE];
     let _ = stream.read_exact(&mut buf).unwrap();
     println!("Received {} bytes", buf.len());
 }
