2bb4ef420769a0289955a3ae8bb11659f2afc1c2
changqi <84911039+comeonsunny@users.noreply.github.com>
2023-10-06 14:21:58 -0400

    Update extract_necessary_information.py
    This is the updated version to extract the necessary information. Fix the regular bug and change the logic way to calculate the setup phase time.

--- extract_necessary_information.py
+++ extract_necessary_information.py
@@ -1,43 +1,42 @@
-# Reading the contents of the uploaded file
-with open("output.txt", "r") as file:
-    lines = file.readlines()
-
-# Extracting all lines that match the pattern
-matching_lines = [line.strip() for line in lines if "Setup Phase took" in line and "amortized time" in line]
-
 import re
 
-# Extracting the amortized time values for "Setup Phase"
-setup_times = [float(re.search(r"Setup Phase took \d+ ms, amortized time (\d+\.\d+) ms", line).group(1))
-               for line in lines if "Setup Phase took" in line]
+# Open and read the uploaded file
+with open("./output.txt", "r") as file:
+    content = file.read()
 
-# Extracting the amortized time values for "Online Phase"
-online_phase_times = [float(re.search(r"Online Phase took \d+ ms, amortized time (\d+\.\d+) ms", line).group(1))
-                      for line in lines if "Online Phase took" in line]
-# Extracting the "End to end amortized time" values
-end_to_end_times = [float(re.search(r"End to end amortized time (\d+\.\d+) msEnd to end", line).group(1))
-                    for line in lines if "End to end amortized time" in line]
+# Define the patterns to search for
+setup_total_time_pattern = r"Setup Phase took ([\d\.]+) ms"
+local_set_num_pattern = r"Local Set Num (\d+)"
+online_pattern = r"Online Phase took [\d\.]+ ms, amortized time ([\d\.]+) ms"
+end_to_end_pattern = r"End to end amortized time ([\d\.]+) ms"
 
+# Extract the required values using regex
+setup_total_times = [float(time) for time in re.findall(setup_total_time_pattern, content)]
+local_set_nums = [float(num) for num in re.findall(local_set_num_pattern, content)]
+online_times = re.findall(online_pattern, content)
+end_to_end_times = re.findall(end_to_end_pattern, content)
 
-# Provided order of (DBEntrySize, N)
-orders = [
-    ("4KB", "2^14"), ("4KB", "2^16"), ("4KB", "2^18"), ("4KB", "2^20"), ("4KB", "2^22"), ("4KB", "2^24"),
-    ("16KB", "2^12"), ("16KB", "2^14"), ("16KB", "2^16"), ("16KB", "2^18"), ("16KB", "2^20"), ("16KB", "2^22"),
-    ("64KB", "2^10"), ("64KB", "2^12"), ("64KB", "2^14"), ("64KB", "2^16"), ("64KB", "2^18"), ("64KB", "2^20"),
-    ("256KB", "2^08"), ("256KB", "2^10"), ("256KB", "2^12"), ("256KB", "2^14"), ("256KB", "2^16"), ("256KB", "2^18")
+# Calculate the new amortized times for the setup phase
+computed_setup_times = [total_time / num for total_time, num in zip(setup_total_times, local_set_nums)]
+
+# Create a list of (DBEntrySize, N) pairs
+dbentry_n_pairs = [
+    "4KB, 2^14", "4KB, 2^16", "4KB, 2^18", "4KB, 2^20", "4KB, 2^22", "4KB, 2^24",
+    "16KB, 2^12", "16KB, 2^14", "16KB, 2^16", "16KB, 2^18", "16KB, 2^20", "16KB, 2^22",
+    "64KB, 2^10", "64KB, 2^12", "64KB, 2^14", "64KB, 2^16", "64KB, 2^18", "64KB, 2^20",
+    "256KB, 2^08", "256KB, 2^10", "256KB, 2^12", "256KB, 2^14", "256KB, 2^16", "256KB, 2^18"
 ]
 
+# Prepare the data to be written to the output file with the updated setup phase times
+output_data_updated = []
+for idx, pair in enumerate(dbentry_n_pairs):
+    output_data_updated.append(f"DBEntrySize: {pair}")
+    output_data_updated.append(f"Setup Phase Amortized Time: {computed_setup_times[idx]} ms")
+    output_data_updated.append(f"Online Phase Amortized Time: {online_times[idx]} ms")
+    output_data_updated.append(f"End to End Amortized Time: {end_to_end_times[idx]} ms")
+    output_data_updated.append("----------")
 
-paired_values_setup = list(zip(orders, setup_times))
-paired_values_online = list(zip(orders, online_phase_times))
-paired_values_end_to_end = list(zip(orders, end_to_end_times))
-
-
-# Saving the results to a file
-with open("output_extracted.txt", "w") as file:
-    for (db_size, n), setup_time, online_time, end_to_end_time in zip(orders, setup_times, online_phase_times, end_to_end_times):
-        file.write(f"DBEntrySize: {db_size}, N: {n} -> "
-                   f"Setup Phase Amortized Time: {setup_time} ms, "
-                   f"Online Phase Amortized Time: {online_time} ms, "
-                   f"End to End Amortized Time: {end_to_end_time} ms\n")
-
+# Write the updated results to a new file
+output_filename_updated = "./extracted_data_updated.txt"
+with open(output_filename_updated, "w") as outfile:
+    outfile.write("\n".join(output_data_updated))
